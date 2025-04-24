785e9127978b13c4f9ffd01e6122e50446f93da0
gerald.kaufman <gerald.kaufman@mymail.champlain.edu>
2025-04-24 12:15:08 -0400

    Implemented base ImGUI options for meeting today

--- Assets/Scripts/UI/ShaderUI.cs
+++ Assets/Scripts/UI/ShaderUI.cs
@@ -4,7 +4,41 @@ using ImGuiNET;
 public class ShaderUI : MonoBehaviour {
 
     private void OnLayout() {
-        ImGui.ShowDemoWindow();
+        if (!ImGui.Begin("Graphics Final")) {
+            ImGui.End();
+            return;
+        }
+        // Render Function
+        RenderUIElements();
+
+        ImGui.End();
+    }
+
+    private void RenderUIElements() {
+        if (ImGui.CollapsingHeader("Grass Variations")) {
+            AddGrass();
+        }
+
+        if (ImGui.CollapsingHeader("Day Night Cycle")) {
+            AddDayNightCycleUI();
+        }
+    }
+
+    private void AddGrass() {
+        AddGrassColorUI();
+        AddGrassSwayUI();
+    }
+
+    private void AddGrassColorUI() {
+
+    }
+
+    private void AddGrassSwayUI() {
+
+    }
+
+    private void AddDayNightCycleUI() {
+
     }
 
     private void OnEnable() {
